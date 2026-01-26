d1b916692a16a274c5822ef5354df9be05b1e98a
Aisc2004 <aisabela.scarvalho@gmail.com>
2026-01-26 11:20:32 -0300

    alt

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ from mainWindows import MainWindows
 from PySide6.QtWidgets import QApplication
 from style import setup_theme
 
+
 if __name__ == '__main__':
+
     app = QApplication(sys.argv)
     setup_theme(app)
     windows = MainWindows()
     windows.show()
     windows.adjust_fixed_size()
     app.exec()
+    
+    
+
+    
+
+ 
+   
